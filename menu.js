d9f0063c5b95ae564662c3f2f2be2ed0928dee75
gumartins77 <gugumartin77@gmail.com>
2024-04-23 13:48:31 -0300

    feat: add glass effect on navbar

--- menu.js
+++ menu.js
@@ -1,15 +1,20 @@
 let btnAbrirMenu = document.getElementById("btn-abrir-menu");
 let menuMobile = document.getElementById("menu-mobile");
 let overlayMenu = document.getElementById("overlay-menu");
+let header = document.querySelector("#header");
 
 btnAbrirMenu.addEventListener("click", () => {
-    menuMobile.classList.add('abrir-menu')
+  menuMobile.classList.add("abrir-menu");
 });
 
 menuMobile.addEventListener("click", () => {
-    menuMobile.classList.remove('abrir-menu')
+  menuMobile.classList.remove("abrir-menu");
 });
 
 overlayMenu.addEventListener("click", () => {
-    menuMobile.classList.remove('abrir-menu')
+  menuMobile.classList.remove("abrir-menu");
+});
+
+window.addEventListener("scroll", () => {
+    header.classList.toggle("rolagem", window.scrollY > 0)
 });
