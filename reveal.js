bd7cf4672746214b29d6e2bae174334ca935607e
gumartins77 <gugumartin77@gmail.com>
2024-04-24 10:52:55 -0300

    feat: add scrollrevealjs

---
+++ reveal.js
@@ -0,0 +1,153 @@
+window.revelar = ScrollReveal({reset:true})
+
+// TOPO DO SITE
+revelar.reveal('.efeito-txt-topo', {
+    duration: 1000,
+    distance: '90px'
+})
+
+revelar.reveal('.efeito-img-topo', {
+    duration: 1000,
+    distance: '90px',
+    delay: 500
+})
+
+// TITULOS DAS SESSÕES
+
+revelar.reveal('.titulo', {
+    duration: 1000,
+    distance: '90px'
+})
+
+// SESSÃO DE ESPECIALIDADES 
+
+revelar.reveal('.efeito-especialidades-1', {
+    duration: 1000,
+    distance: '90px',
+    delay: 300
+})
+
+revelar.reveal('.efeito-especialidades-2', {
+    duration: 1000,
+    distance: '90px',
+    delay: 500
+})
+
+revelar.reveal('.efeito-especialidades-3', {
+    duration: 1000,
+    distance: '90px',
+    delay: 700
+})
+
+// SESSÃO SOBRE
+
+revelar.reveal('.efeito-img-sobre', {
+    duration: 1000,
+    distance: '50px',
+    origin: 'left'
+})
+
+revelar.reveal('.efeito-h2-sobre', {
+    duration: 1000,
+    distance: '50px',
+    delay: 300,
+    origin: 'right'
+})
+
+revelar.reveal('.efeito-span-sobre', {
+    duration: 1000,
+    distance: '50px',
+    delay: 400,
+    origin: 'right'
+})
+
+revelar.reveal('.efeito-p-1-sobre', {
+    duration: 1000,
+    distance: '50px',
+    delay: 500,
+    origin: 'right'
+})
+
+revelar.reveal('.efeito-p-2-sobre', {
+    duration: 1000,
+    distance: '50px',
+    delay: 600,
+    origin: 'right'
+})
+
+revelar.reveal('.efeito-button-1-sobre', {
+    duration: 1000,
+    distance: '50px',
+    delay: 700,
+    origin: 'right'
+})
+
+revelar.reveal('.efeito-button-2-sobre', {
+    duration: 1000,
+    distance: '50px',
+    delay: 800,
+    origin: 'right'
+})
+
+revelar.reveal('.efeito-button-3-sobre', {
+    duration: 1000,
+    distance: '50px',
+    delay: 900,
+    origin: 'right'
+})
+
+revelar.reveal('.efeito-button-4-sobre', {
+    duration: 1000,
+    distance: '50px',
+    delay: 1000,
+    origin: 'right'
+})
+
+// SESSÃO PORTFOLIO
+
+revelar.reveal('.efeito-img-port-1', {
+    duration: 1000,
+    distance: '90px',
+    delay: 300,
+    origin: 'bottom'
+})
+
+revelar.reveal('.efeito-img-port-2', {
+    duration: 1000,
+    distance: '90px',
+    delay: 600,
+    origin: 'bottom'
+})
+
+// SESSÃO FORMULARIO
+
+revelar.reveal('.efeito-input-form-1', {
+    duration: 1000,
+    distance: '90px',
+    delay: 300
+})
+
+revelar.reveal('.efeito-input-form-2', {
+    duration: 1000,
+    distance: '90px',
+    delay: 400
+})
+
+revelar.reveal('.efeito-input-form-3', {
+    duration: 1000,
+    distance: '90px',
+    delay: 500
+})
+
+revelar.reveal('.efeito-input-form-4', {
+    duration: 1000,
+    distance: '90px',
+    delay: 600
+})
+
+revelar.reveal('.efeito-btn-enviar', {
+    duration: 1000,
+    distance: '90px',
+    delay: 700
+})
+
